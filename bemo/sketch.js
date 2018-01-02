03156b852a45e1ae567bf8dc0c35900764ed8eec
Esmond <>
2018-01-01 17:25:47 -0800

    first  commit

---
+++ bemo/sketch.js
@@ -0,0 +1,46 @@
+                            s 
+
+function preload() 
+{
+
+}
+
+function setup() 
+{
+	createCanvas(640, 480);
+}
+
+
+function draw() 
+{
+background(255, 0, 0);
+
+	//strokeWeight(15);
+	//stroke(255, 255, 255);
+	//fill(200, 0, 255);
+	//ellipse(mouseX, mouseY, 230, 230);
+	strokeWeight(3);
+	stroke(0, 0, 0);
+	fill(103, 189, 176);
+	 
+rect(0, 0, 270, 450);
+strokeWeight(3);
+	stroke(0, 0, 0);
+	fill(210, 252, 228);
+	
+rect(30, 60, 210, 150);	
+strokeWeight(1);
+	stroke(0, 0, 0);
+	fill(22, 57, 53);
+	  
+
+rect(30, 225, 125, 6);
+strokeWeight(2);
+	stroke(0, 0, 0);
+	fill(36, 32, 196);
+
+ellipse(180, 230, 8);
+
+
+
+}
